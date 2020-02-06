da985ff760ba1a6c96f16cace245f1abba8356c3
LiamHua <liamhuazai@gmail.com>
2020-02-06 17:19:27 +0800

    完成基本构造

---
+++ src/components/user/User.vue
@@ -0,0 +1,103 @@
+<template>
+  <div>
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+      <el-breadcrumb-item :to="{ path: '/home' }"><span @click="changeActivePath">首页</span></el-breadcrumb-item>
+      <el-breadcrumb-item>用户管理</el-breadcrumb-item>
+      <el-breadcrumb-item>用户列表</el-breadcrumb-item>
+    </el-breadcrumb>
+    <el-card>
+      <el-row :gutter="30">
+        <el-col :span="5">
+          <el-input placeholder="请输入内容">
+            <el-button slot="append" icon="el-icon-search"></el-button>
+          </el-input>
+        </el-col>
+        <el-col :span="1">
+          <el-button type="primary">添加用户</el-button>
+        </el-col>
+      </el-row>
+      <el-table :data="tableData" style="width: 100%" border stripe>
+        <el-table-column type="index" label="#"></el-table-column>
+        <el-table-column label="姓名" prop="name"></el-table-column>
+        <el-table-column label="邮箱" prop="name"></el-table-column>
+        <el-table-column label="电话" prop="name"></el-table-column>
+        <el-table-column label="角色" prop="name"></el-table-column>
+        <el-table-column label="状态">
+          <template slot-scope="scope">
+            <el-switch
+              v-model="scope.row.status"
+              active-color="#13ce66"
+              inactive-color="#ff4949">
+            </el-switch>
+          </template>
+        </el-table-column>
+        <el-table-column label="操作">
+          <template>
+            <el-button type="primary" icon="el-icon-edit" size="small"></el-button>
+            <el-button type="danger" icon="el-icon-delete" size="small"></el-button>
+            <el-tooltip effect="dark" content="分配角色" placement="top" :enterable="false">
+              <el-button type="warning" icon="el-icon-setting" size="small"></el-button>
+            </el-tooltip>
+          </template>
+        </el-table-column>
+      </el-table>
+    </el-card>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Users',
+    data () {
+      return {
+        tableData: [{
+          date: '2016-05-02',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄',
+          status: true
+        }, {
+          date: '2016-05-04',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1517 弄',
+          status: true
+        }, {
+          date: '2016-05-01',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1519 弄',
+          status: true
+        }, {
+          date: '2016-05-03',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1516 弄',
+          status: false
+        }]
+      }
+    },
+    methods: {
+      changeActivePath () {
+        window.sessionStorage.setItem('activePath', '/welcome')
+        this.$parent.party.activepath = '/welcome'
+      }
+    }
+  }
+</script>
+
+<style lang="less" scoped>
+  .el-breadcrumb {
+    font-size: 15px;
+  }
+
+  .el-card {
+    margin-top: 20px;
+    box-shadow: 0 1px 1px rgba(0, 0, 0, 0.15) !important;
+  }
+
+  .el-table {
+    margin-top: 20px;
+
+    .el-button {
+      margin-right: 15px;
+    }
+  }
+
+</style>
